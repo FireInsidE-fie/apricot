843b07e54f118e1ecf829170692e8f9156fb9f75
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-10-31 18:41:04 +0100

    add frees and testing for the RIFF parsing

--- src/wav/wav.c
+++ src/wav/wav.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	read_wav_head(char *path)
+t_header	*read_wav_head(char *path)
 {
 	FILE		*file;
 	t_header	*header;
@@ -11,18 +11,24 @@ int	read_wav_head(char *path)
 	file = fopen(path, "r");
 	header = malloc(sizeof(t_header));
 	if (!file)
-		return (-1);
+	{
+		free(header);
+		return (NULL);
+	}
 	// Parsing the header in the t_header struct
 	fread(header->riff, 1, 4, file); // RIFF string
 	// Wrapping up
 	fclose(file);
-	return (0);
+	return (header);
 }
 
 int	main(int argc, char **argv)
 {
+	t_header	*header;
 	if (argc != 2)
 		return (-1);
-	read_wav_head(argv[1]);
+	header = read_wav_head(argv[1]);
+	printf("%s", header->riff);
+	free(header);
 	return (0);
 }
