39f6dfeb06bf9ebc7e244ba6b3023c5cd6a87c25
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-01 22:28:34 +0100

    add parsing of overall_size

--- src/wav/wav.c
+++ src/wav/wav.c
@@ -8,7 +8,7 @@ t_header	*read_wav_head(char *path)
 	FILE		*file;
 	t_header	*header;
 	int			buffer4[4] = {0};
-	int			i;
+	//int			i;
 
 	file = fopen(path, "r");
 	header = malloc(sizeof(t_header));
@@ -19,10 +19,8 @@ t_header	*read_wav_head(char *path)
 	}
 	// Parsing the header in the t_header struct
 	fread(header->riff, sizeof(header->riff), 1, file); // RIFF string
-	fread(buffer4, 4, 1, file);
-	i = 0;
-	while (i < 4)
-		printf("%d", buffer4[i++]);
+	fread(buffer4, 4, 1, file); // overall_size
+	header->overall_size = buffer4[0] | buffer4[1]<<8 | buffer4[2]<<16 | buffer4[3]<<24;
 	// Wrapping up
 	fclose(file);
 	return (header);
@@ -31,10 +29,12 @@ t_header	*read_wav_head(char *path)
 int	main(int argc, char **argv)
 {
 	t_header	*header;
+
 	if (argc != 2)
 		return (-1);
 	header = read_wav_head(argv[1]);
 	printf("%s", header->riff);
+	printf("%u", header->overall_size);
 	free(header);
 	return (0);
 }
